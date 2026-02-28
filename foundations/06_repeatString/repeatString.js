09329445f704615d56f8f49c15f16296afc9ee7a
Morris <underwater_forests@outlook.com>
2026-02-27 21:36:19 -0500

    complete exercise 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let newStr = "";
+    let newArr = [];
 
+    if (num < 0) {
+        return `Error`
+    } else {
+        for (let i = 0; i < num; i++) {
+            newArr.push(str);
+        }
+        return newStr = newArr.join("");
+    }
 };
+
+// console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;

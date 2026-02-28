9ae0ccf62ddb3cde20ac29886f2f1b35fb50e02a
Morris <underwater_forests@outlook.com>
2026-02-28 18:34:17 -0500

    compete 08 exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, firstElement,...otherElements) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (firstElement === arr[i] || otherElements.includes(arr[i])) {
+            arr.splice(i, 1);
+        }
+    }
+
+    return arr;
 };
+
+let arr = [1,2,3,4]
+console.log(removeFromArray(arr, 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;

a1b95c6abef8b1085a9b56bbf5c2bb22e9a0b305
Morris <underwater_forests@outlook.com>
2026-02-27 22:13:17 -0500

    complete exercise 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,24 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let arr = [];
+    let revArr = [];
+    let revStr = "";
 
+    for (let i = 0; i < str.length ; i++) {
+        arr.push(str.charAt(i));
+    }
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        revArr.push(arr[i]);
+    }
+
+    revStr = revArr.join('');
+
+    // console.log(revStr);
+
+    return revStr;
 };
+
+reverseString("Hello, World!");
 
 // Do not edit below this line
 module.exports = reverseString;
